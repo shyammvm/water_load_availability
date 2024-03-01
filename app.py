d06d2de7187c68908a76129885cd84125de6cc01
ShyamMohan V M <shyammohan.vm@factspan.blr>
2024-03-01 16:45:42 +0530

    flask worked for the first time

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def search():
     if request.method == 'POST':
         dropdown_value = request.form['dropdown']
         search_text = request.form['search_text']
         # Perform search or any other action here
-        print(f"Dropdown Value: {dropdown_value}, Search Text: {search_text}")
+        # print(f"Dropdown Value: {dropdown_value}, Search Text: {search_text}")
     return render_template('index.html')
 
 if __name__ == '__main__':
